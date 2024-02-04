343e175835284bfb560169cd506ce72c2b81fc56
EndritHay <[hajrullahuend@gmail.com]>
2024-02-04 22:09:35 +0100

    add-product_UI

--- ecommerce/src/components/utils/index.js
+++ ecommerce/src/components/utils/index.js
@@ -38,7 +38,7 @@ export const navOptions = [
       path: "/admin-view/add-product",
     },
   ];
- 
+
   //proper inputeve te register/login
   export const registrationFormControls = [
  
@@ -61,7 +61,8 @@ export const navOptions = [
       placeholder : 'Enter Your password',
       label: 'Password',
       componentType : 'input'
-    },   {
+    },   
+    {
       id: 'role',
       type : '',
       placeholder : '',
@@ -75,10 +76,9 @@ export const navOptions = [
         {
           id : 'costumer',
           label : 'Costumer', 
-          }
-      ]
-
-    }
+          },
+      ],
+    },
   ]
   export const loginFormControls = [
     {
@@ -96,5 +96,96 @@ export const navOptions = [
       label: 'Password',
       componentType: 'input'
     }
-
   ]
+    //inputet e addProduct faqes
+
+    export const adminAddProductformControls = [
+      {
+        id: "name",
+        type: "text",
+        placeholder: "Enter name",
+        label: "Name",
+        componentType: "input",
+      },
+      {
+        id: "price",
+        type: "number",
+        placeholder: "Enter price",
+        label: "Price",
+        componentType: "input",
+      },
+      {
+        id: "description",
+        type: "text",
+        placeholder: "Enter description",
+        label: "Description",
+        componentType: "input",
+      },
+      {
+        id: "category",
+        type: "",
+        placeholder: "",
+        label: "Category",
+        componentType: "select",
+        options: [
+          {
+            id: "men",
+            label: "Men",
+          },
+          {
+            id: "women",
+            label: "Women",
+          },
+          {
+            id: "kids",
+            label: "Kids",
+          },
+        ],
+      },
+      {
+        id: "deliveryInfo",
+        type: "text",
+        placeholder: "Enter deliveryInfo",
+        label: "Delivery Info",
+        componentType: "input",
+      },
+      {
+        id: "onSale",
+        type: "",
+        placeholder: "",
+        label: "On Sale",
+        componentType: "select",
+        options: [
+          {
+            id: "yes",
+            label: "Yes",
+          },
+          {
+            id: "no",
+            label: "No",
+          },
+        ],
+      },
+      {
+        id: "priceDrop",
+        type: "number",
+        placeholder: "Enter Price Drop",
+        label: "Price Drop",
+        componentType: "input",
+      },
+    ];
+  
+    export const AvailableSizes = [
+      {
+        id : 's',
+        label: 'S'
+      },
+      {
+        id : 'm',
+        label: 'M'
+      },
+      {
+        id : 'l',
+        label: 'L'
+      }
+    ]
